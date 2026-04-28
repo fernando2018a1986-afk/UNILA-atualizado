889cba1f94d9d46e40fa0db4b8a7335ed8dbbd17
fernando2018a1986-afk <fernando2018a1986@gmail.com>
2026-04-27 21:40:20 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,43 @@
+const CACHE_NAME = "unila-cache-v3";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icone-192.png",
+  "./icone-512.png"
+];
+
+// INSTALAR
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open("unila-cache-v4").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// ATIVAR
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH (ESTRATÉGIA CORRETA)
 self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    // SEMPRE pega a versão nova do index.html
+    event.respondWith(fetch(event.request).catch(() => caches.match("./index.html")));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
